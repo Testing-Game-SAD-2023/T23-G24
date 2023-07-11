b5553f80ec9e9815e2a5d5bc1ae114adae96e9a5
Franceware <95299390+Franceware@users.noreply.github.com>
2023-07-11 15:26:01 +0200

    Delete AuthenticationG24 directory

--- AuthenticationG24/src/main/java/com/g24/authentication/model/service/TokenService.java
+++
@@ -1,9 +0,0 @@
-//package com.g24.authentication.model.service;
-//
-//import com.g24.authentication.model.entity.Token;
-//import com.g24.authentication.model.entity.User;
-//
-//public interface TokenService {
-//
-//	public Token createToken (User user, String type);
-//}
